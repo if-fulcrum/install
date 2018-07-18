c8941e5e10a2e76419512d1080ffa286373d5541
Kenn Herman <kenn@ifsight.com>
2018-07-18 15:52:19 -0700

    unified unix script

---
+++ unix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# for bootstrap we at least want
+if (which git > /dev/null); then
+  # clone fulcrum if needed
+  if [ ! -d ~/fulcrum ]; then
+    git -C ~/ clone https://github.com/if-fulcrum/fulcrum.git
+  fi
+
+  # pull no matter what
+  cd ~/fulcrum
+  git pull
+
+  # bring up fulcrum, doctor should get us into place
+  # ~/fulcrum/bin/fulcrum up
+  ~/fulcrum/bin/doctor2
+else
+  echo "[fulcrum/install] ERROR: You must have git installed to continue, please install and try again"
+  exit 1
+fi
+
