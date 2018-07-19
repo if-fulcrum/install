20fb4bafda19b6bb21d65b87e4f0eb03dcb017db
Kenn Herman <kenn@ifsight.com>
2018-07-18 19:25:34 -0700

    use fulcrum2

--- unix.sh
+++ unix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# curl -fsSL https://raw.githubusercontent.com/if-fulcrum/install/master/unix.sh | bash
+
 # for bootstrap we at least want
 if (which git > /dev/null); then
   # clone fulcrum if needed
@@ -12,8 +14,8 @@ if (which git > /dev/null); then
   git pull
 
   # bring up fulcrum, doctor should get us into place
-  # ~/fulcrum/bin/fulcrum up
-  ~/fulcrum/bin/doctor2
+  ~/fulcrum/bin/fulcrum2 up
+  # ~/fulcrum/bin/doctor2
 else
   echo "[fulcrum/install] ERROR: You must have git installed to continue, please install and try again"
   exit 1
