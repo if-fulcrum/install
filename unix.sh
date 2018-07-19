03133a7aa9967cea0145993f8bc2b540ecc41328
Kenn Herman <kenn@ifsight.com>
2018-07-18 21:48:33 -0700

    if typo

--- unix.sh
+++ unix.sh
@@ -28,7 +28,7 @@ function getPrerequisites() {
     if [[ "$UNAMEA" == *"Darwin"* ]]; then
       if ! (which brew > /dev/null); then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-      else
+      fi
 
       brew install git
     elif [[ "$UNAMEA" == *"Linux"* ]]; then
