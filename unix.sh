d6dccd2a87dda13cab2a4a213c9cc7bda21cb0b7
Kenn Herman <kenn@ifsight.com>
2018-07-19 22:14:20 -0700

    make brew what needs to be installed on macOS

--- unix.sh
+++ unix.sh
@@ -30,34 +30,35 @@ function main() {
 }
 
 function getPrerequisites() {
-  if ! (which git > /dev/null); then
-    UNAMEA=$(uname -a)
+  UNAMEA=$(uname -a)
 
-    if [[ "$UNAMEA" == *"Darwin"* ]]; then
-      if ! (which brew > /dev/null); then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-      fi
+  # macOS: need Brew, will include git via Xcode
+  # `which git` bad since it could pop macOS Xcode Command Line Tools dialog box
+  if [[ "$UNAMEA" == *"Darwin"* ]]; then
+    if ! (which brew > /dev/null); then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+  # Linux: need git
+  elif [[ "$UNAMEA" == *"Linux"* ]]; then
+    if [ -e /etc/os-release ]; then
+      source /etc/os-release
+      OS_DISTRO=$(echo $ID|tr '[:upper:]' '[:lower:]'|tr ' ' '-')
 
-      brew install git
-    elif [[ "$UNAMEA" == *"Linux"* ]]; then
-      if [ -e /etc/os-release ]; then
-        source /etc/os-release
-        FULCRUM_OS_DISTRO=$(echo $ID|tr '[:upper:]' '[:lower:]'|tr ' ' '-')
-
-        if [[ "$FULCRUM_OS_DISTRO" == "debian" || "$FULCRUM_OS_DISTRO" == "ubuntu" ]]; then
+      if [[ "$OS_DISTRO" == "debian" || "$OS_DISTRO" == "ubuntu" ]]; then
+        if ! (which git > /dev/null); then
           sudo apt install -y git
-        else
-          echo "ERROR: Linux distro $FULCRUM_OS_DISTRO not supported"
-          exit 1
         fi
       else
-        echo "ERROR: Cann not identify Linux distro"
+        echo "ERROR: Linux distro $OS_DISTRO not supported"
         exit 1
       fi
     else
-      echo "ERROR: OS $UNAMEA not supported"
+      echo "ERROR: Can not identify Linux distro"
       exit 1
     fi
+  else
+    echo "ERROR: OS $UNAMEA not supported"
+    exit 1
   fi
 }
 
